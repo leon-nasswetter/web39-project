a1e1f6f58da59f27b5cdf0b92b996967998a4d48
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-03-11 10:39:44 -0800

    wip

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 require("dotenv").config()
-
+const path = require("path")
 const express = require("express")
 
 const server = express()
 
 server.use(express.json())
+server.use(express.static(path.join(__dirname, "client/build")))
 
 if(process.env.NODE_ENV === "development"){ // on heroku machine, an env variable is called "NODE_ENV" and has value production
     const cors = require("cors")
     server.use(cors())
 }
 
-server.use("*", (req, res) => {
-    res.send("<h1>success</h1>")
+// our api comes earlier in the pipeline
+server.get("api/hello", (req, res) => {
+    res.json({ message: "hello" })
+})
+
+// catch all that will send back to index.html
+server.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"))
 })
 
 const PORT = process.env.PORT || 4000
